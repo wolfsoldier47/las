afebc461cddf02f7ebfb19260ad4273dfa119b45
SoNik <khalidfaraj8@gmail.com>
2026-09-18 12:18:40 +0800

    Update hcv.go

---
+++ models/hcv.go
@@ -0,0 +1,26 @@
+package models
+
+import "ulas-service/internal/client/httpi"
+
+type Auth struct {
+	ClientToken string `json:"client_token"`
+}
+
+type AppRoleResponse struct {
+	Auth Auth `json:"auth"`
+}
+
+type AppRoleRequest struct {
+	RoleId   string `json:"role_id"`
+	SecretId string `json:"secret_id"`
+}
+
+type Credential struct {
+	Username string `yaml:"username_new"`
+	Password string `yaml:"password_new"`
+}
+
+type HcvService interface {
+	Token(url string, namespace string, roleId string, secretId string) (string, error)
+	HttpClient(token string) httpi.Client
+}
