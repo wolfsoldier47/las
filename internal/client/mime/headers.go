3c67bbf0aff84655841db7d55af9d7042a1a3b5c
SoNik <khalidfaraj8@gmail.com>
2026-09-18 12:14:38 +0800

    Update headers.go

---
+++ internal/client/mime/headers.go
@@ -0,0 +1,11 @@
+package mime
+
+const (
+	HeaderContentType = "Content-Type"
+	HeaderUserAgent   = "User-Agent"
+
+	ContentTypeJson        = "application/json"
+	ContentTypeXml         = "application/xml"
+	ContentTypeOctetStream = "application/octet-stream"
+)
+
